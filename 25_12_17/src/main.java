35465ed4165c433f78291e07466f407e59b76509
simonliebetrau <simlie135@icloud.com>
2025-12-17 21:15:09 +0100

    refactor encoding logic and update Morse code handling

--- 25_12_17/src/main.java
+++ 25_12_17/src/main.java
@@ -1,10 +1,12 @@
+import Common.Queue;
+
 public class main {
     private static char[] characters = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' '};
-    private static String[] morse = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..", " "};
+    private static String[] morse = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..", "_"};
 
     public static void main(String[] args) {
         System.out.println(decode("Hallo Welt"));
-        System.out.println(encode(".... .- .-.. .-.. --- null .-- . .-.. -"));
+        System.out.println(encode(".... .- .-.. .-.. --- _ .-- . .-.. - "));
     }
 
     public static String decode(String pNachricht) {
@@ -22,12 +24,24 @@ public class main {
     }
 
     public static String encode(String pNachricht) {
-        for(int i = 0; i < morse.length; i++) {
-            pNachricht.replaceAll(morse[i], ""+characters[i]);
+        Queue<String> nachricht = new Queue<>();
+
+        int charStartIndex = 0;
+
+        for(int i = 0; i < pNachricht.length(); i++) {
+            if(pNachricht.charAt(i) == ' ') {
+                String morsePart = pNachricht.substring(charStartIndex, i);
+                for(int j = 0; j < morse.length; j++) {
+                    if(morsePart.equals(morse[j])) {
+                        nachricht.enqueue("" + characters[j]);
+                    }
+                }
+            charStartIndex = i + 1;
+            }
+
         }
 
-        return pNachricht;
-
+        return uebersetzeInString(nachricht);
     }
 
     public static String uebersetzeInString(String[] pNachricht) {
@@ -39,4 +53,13 @@ public class main {
         return nachricht;
     }
 
+    public static String uebersetzeInString(Queue<String> pNachricht) {
+        String nachricht = "";
+        while (!pNachricht.isEmpty()) {
+            nachricht = nachricht + pNachricht.front();
+            pNachricht.dequeue();
+        }
+
+        return nachricht;
+    }
 }
